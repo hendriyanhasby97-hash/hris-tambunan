09a93f34ecdc7e4ff595971bb3ca7b0898f29508
hendriyanhasby97-hash <hendriyanhasby.97@gmail.com>
2026-06-10 13:52:37 +0700

    Update index.js

--- Layout/Employee/index.js
+++ Layout/Employee/index.js
@@ -1,1 +1,19 @@
+// Layout/index.js
+import { supabase } from '../App/koneksi.js';
 
+document.addEventListener('DOMContentLoaded', async () => {
+    // Tampilkan nama user aktif di header
+    const { data: { user } } = await supabase.auth.getUser();
+    if (user) {
+        document.getElementById('user-email').innerText = user.email;
+    }
+
+    // Handle Tombol Logout
+    const btnLogout = document.getElementById('btn-logout');
+    if (btnLogout) {
+        btnLogout.addEventListener('click', async () => {
+            await supabase.auth.signOut();
+            window.location.href = '/login.html';
+        });
+    }
+});
