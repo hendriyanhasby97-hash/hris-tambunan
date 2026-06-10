73665932270c470ca38fd7387bf6474b861b87da
hendriyanhasby97-hash <hendriyanhasby.97@gmail.com>
2026-06-10 13:49:04 +0700

    Update koneksi.js

--- App/koneksi.js
+++ App/koneksi.js
@@ -1,1 +1,7 @@
+// App/koneksi.js
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
+const SUPABASE_URL = 'https://rjdymyzeujfxzqwwhxbb.supabase.co'; // Ganti dengan URL Supabase-mu
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJqZHlteXpldWpmeHpxd3doeGJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA2MjI2OTQsImV4cCI6MjA5NjE5ODY5NH0.dUvvgTawV0ZoOGy8VFJ91GkxNufqcITRBAiSoQ3q3tc'; // Ganti dengan Anon Key-mu
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
